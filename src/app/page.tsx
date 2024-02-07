68a0bf0cece73e80aa1dcd2645acb0d78a0c0df0
Joshua Austin <83935880+joshaustintech@users.noreply.github.com>
2024-02-06 20:13:02 -0600

    Added demo API endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  //const appId: string | undefined = process.env.META_APP_ID
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>My First Vercel App</h1>
+      <p>Can you find the JSON endpoint?</p>
     </main>
   );
 }
